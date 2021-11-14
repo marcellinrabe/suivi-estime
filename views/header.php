718973da43dd8f4ca4d2f61fe0fa999a41f606b6
= <=>
2021-11-14 18:39:30 +0300

    secund version backend incomplete

--- views/header.php
+++ views/header.php
@@ -5,9 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>test iteams</title>
+    <title>suivi-estime</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets//css/fontawesome/css/all.css">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/LOGO iteams.png">
 
     <script> 
         let frames = [];
